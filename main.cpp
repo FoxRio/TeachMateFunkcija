7f4ed9433bc6bb7e7967132e9338559aa40e9fdd
FoxRio <davis.krasts@gmail.com>
2024-03-06 17:50:31 +0200

    init: initial main.cpp version

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by davis on 06.03.2024.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by davis on 06.03.2024.
+//
+
+#ifndef TEACHMATEFUNKCIJA_MAIN_H
+#define TEACHMATEFUNKCIJA_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //TEACHMATEFUNKCIJA_MAIN_H
